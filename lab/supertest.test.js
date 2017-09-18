0ba8a817f0b47e4f5bdf642c205221665774f004
David Pham <davidpham5@gmail.com>
2017-09-17 22:34:23 -0400

    add describe test suite

--- lab/supertest.test.js
+++ lab/supertest.test.js
@@ -3,32 +3,39 @@ const expect = require('expect');
 
 var app = require('./supertest').app;
 
-it('should return hello world response', (done) => {
-    request(app)
-        .get('/')
-        .expect(404)
-        // .expect('hello world')
-        // .expect({
-        //     error: 'Page not found'
-        // })
-        .expect( (response) => {
-            // This expect() comes from the const.
-            expect(response.body).toInclude({
-                error: 'Page not found'
-            });
-        })
-        .end(done);
-});
-
-it('should expect users name and age', (done) => {
-    request(app)
-        .get('/users')
-        .expect(200)
-        .expect( (resp) => {
-            expect(resp.body).toInclude({
-                name: 'David',
-                age: 33
-            })
-        })
-        .end(done);
+// Describe() allows to group several tests under one category
+describe('server', () => {
+    describe('get home', () => {
+        it('should return hello world response', (done) => {
+            request(app)
+                .get('/')
+                .expect(404)
+                // .expect('hello world')
+                // .expect({
+                //     error: 'Page not found'
+                // })
+                .expect( (response) => {
+                    // This expect() comes from the const.
+                    expect(response.body).toInclude({
+                        error: 'Page not found'
+                    });
+                })
+                .end(done);
+        });
+    });
+    
+    describe('get /users', () => {
+        it('should expect users name and age', (done) => {
+            request(app)
+                .get('/users')
+                .expect(200)
+                .expect( (resp) => {
+                    expect(resp.body).toInclude({
+                        name: 'David',
+                        age: 33
+                    })
+                })
+                .end(done);
+        });
+    });
 });
