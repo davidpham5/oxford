154686f66eb7d5e133544fd6f7e0e4b635d1e881
David Pham <davidpham5@gmail.com>
2017-09-18 22:12:43 -0400

    add mongo delete and find

--- todo-api/lab/mongodb-connect.js
+++ todo-api/lab/mongodb-connect.js
@@ -1,7 +1,22 @@
 // start connecting to the DB
 
+// es6 object destructioning: pull out object's property and make it a variable
+var user = {
+    name: 'David',
+    age: 33
+}
+// object destruction
+var {name} = user;
+//console.log(name);
+
 // look for mongo client
-const MongoClient = require('mongodb').MongoClient;
+//const MongoClient = require('mongodb').MongoClient;
+// same as above
+const {MongoClient, ObjectID} = require('mongodb');
+
+var obj = new ObjectID();
+// random object id
+// console.log(obj);
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     if (err) {
@@ -23,14 +38,15 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     
     // insert new doc into Users collection (name, age, location)
     db.collection('Users').insertOne({
-        name: 'David',
-        age: 33,
+        name: 'Leo',
+        age: 8,
         location: 'Silver Spring'
     }, (error, result) => {
         if (error) {
             return console.log('Unable to insert User', error);
         }
         // Success
+       // console.log(JSON.stringify(result.ops, undefined, 2));
         console.log(JSON.stringify(result.ops, undefined, 2));
     })
     // close connection
