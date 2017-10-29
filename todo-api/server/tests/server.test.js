f32028d7638d3c9cda14300f23680dd9ec9f7a17
David Pham <davidpham5@gmail.com>
2017-10-28 23:10:24 -0400

    add new describe, get todos with dummy data

--- todo-api/server/tests/server.test.js
+++ todo-api/server/tests/server.test.js
@@ -4,12 +4,31 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todos');
 
+// Dummy todos for our GET method
+// only need the text property because
+// mongoose db will ppoulate the rest
+const todos = [
+    {
+        text: 'test todo text'
+    },
+    {
+        text: 'test todo text again'
+    }
+]
+
 // Lifecycle Method
 // make sure the debabase is empty
 beforeEach((done) => {
+    // Todo.remove({
+    //     // wipe all todos with empty object 
+    // }).then(() => done());
+
+    // insertMany
     Todo.remove({
-        // wipe all todos with empty object 
-    }).then(() => done());
+        // wipe all todos with empty object
+    }).then( () => {
+        return Todo.insertMany(todos);
+    }).then( () => done());
 });
 
 describe('POST /todos', () => {
@@ -31,8 +50,8 @@ describe('POST /todos', () => {
                     return done(error);
                 }
 
-                Todo.find().then((todos) => {
-                    expect(todos.length).toBe(1);
+                Todo.find(text).then((todos) => {
+                    expect(todos.length).toBe(3);
                     expect(todos[0].text).toBe(text);
                     done();
                 }).catch((error) => done(error));
@@ -52,7 +71,7 @@ describe('POST /todos', () => {
                 }
 
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((error) => {
                     done(error);
@@ -60,3 +79,16 @@ describe('POST /todos', () => {
             });
     })
 });
+
+
+describe('GET /todos', () => {
+    it('should get all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((resp) => {
+                expect((resp.body.todos).length).toBe(2);
+            })
+            .end(done);
+    })
+})
